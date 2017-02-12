b64cc2a945b94eca47ba633e573a77e94f5b723a
Charles Vinette <vinettecharles@gmail.com>
2017-02-11 22:52:23 -0500

    Finished example

--- exponent-auth0/main.js
+++ exponent-auth0/main.js
@@ -5,22 +5,59 @@ import {
   Text,
   View,
   Button,
+  Linking,
 } from 'react-native';
+import jwtDecoder from 'jwt-decode';
 
-const redirect_uri = 'host.exp.exponent.oauth://redirect'
-const auth0_client_id = 'pdnNOE8axmLRPk6opnr6pSbIxmFJxAlA'
+const redirect_uri = 'exp://e8-j5w.charlesvinette.exponent-auth0.exp.direct/+/redirect';
+const auth0_client_id = 'pdnNOE8axmLRPk6opnr6pSbIxmFJxAlA';
 
 class App extends React.Component {
+  state = {
+    username: undefined,
+  };
+  componentDidMount() {
+    Linking.addEventListener('url', this._handleAuth0Redirect);
+  }
 
   _loginWithAuth0 = async () => {
-    let redirectionURL = 'https://charlesvinette.auth0.com/authorize' + this._toQueryString({
-      client_id:auth0_client_id,
-      response_type:'token',
-      scope:'openid name',
-      redirect_uri:redirect_uri,
-      scope:'yeehoo'
-    })
+    const redirectionURL = 'https://charlesvinette.auth0.com/authorize' + this._toQueryString({
+      client_id: auth0_client_id,
+      response_type: 'token',
+      scope: 'openid name',
+      redirect_uri,
+      state: redirect_uri,
+    });
     Exponent.WebBrowser.openBrowserAsync(redirectionURL);
+  }
+
+  _loginWithAuth0Twitter = async () => {
+    const redirectionURL = 'https://charlesvinette.auth0.com/authorize' + this._toQueryString({
+      client_id: auth0_client_id,
+      response_type: 'token',
+      scope: 'openid name',
+      redirect_uri,
+      connection: 'twitter',
+      state: redirect_uri,
+    });
+    Exponent.WebBrowser.openBrowserAsync(redirectionURL);
+  }
+
+  _handleAuth0Redirect = async (event) => {
+    if (!event.url.includes('+/redirect')) {
+      return;
+    }
+    Exponent.WebBrowser.dismissBrowser();
+    const [, queryString] = event.url.split('#');
+    const responseObj = queryString.split('&').reduce((map, pair) => {
+      const [key, value] = pair.split('=');
+      map[key] = value; // eslint-disable-line
+      return map;
+    }, {});
+    const encodedToken = responseObj.id_token;
+    const decodedToken = jwtDecoder(encodedToken);
+    const username = decodedToken.name;
+    this.setState({ username });
   }
 
   /**
@@ -35,8 +72,15 @@ class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Example: Auth0 login</Text>
-        <Button title="Login with Auth0" onPress={this._loginWithAuth0}/>
+        {this.state.username !== undefined ?
+          <Text style={styles.title}>Hi {this.state.username}!</Text> :
+          <View>
+            <Text style={styles.title}>Example: Auth0 login</Text>
+            <Button title="Login with Auth0" onPress={this._loginWithAuth0} />
+            <Text style={styles.title}>Example: Auth0 force Twitter</Text>
+            <Button title="Login with Auth0-Twitter" onPress={this._loginWithAuth0Twitter} />
+          </View>
+        }
       </View>
     );
   }
